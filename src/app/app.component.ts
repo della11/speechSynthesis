0d1fa7e2cb8b109e2e8cb4e073b5f13e0b3f6df4
fabio.dellarosa <fabio.dellarosa@alten.it>
2023-08-17 16:00:38 +0200

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'speechSynthesis';
+
+	public selectedRate: number;
+	public selectedVoice: SpeechSynthesisVoice | null;
+	public text: string;
+	public voices: SpeechSynthesisVoice[];
+
+	// I initialize the app component.
+	constructor() {
+
+		this.voices = [];
+		this.selectedVoice = null;
+		this.selectedRate = 1;
+    this.text = "Lautaro Martinez inter 100 crediti  offerti da Fabio ";
+	}
+
+	// ---
+	// PUBLIC METHODS.
+	// ---
+	// I get called once after the inputs have been bound for the first time.
+	public ngOnInit() : void {
+
+		this.voices = speechSynthesis.getVoices();
+		this.selectedVoice = ( this.voices[ 0 ] || null );
+	}
+
+
+	// I synthesize speech from the current text for the currently-selected voice.
+	public speak() : void {
+
+		// if ( ! this.selectedVoice || ! this.text ) {
+
+		// 	return;
+
+		// }
+
+    this.voices = speechSynthesis.getVoices();
+    this.selectedVoice = this.voices[1];
+
+		this.stop();
+		this.synthesizeSpeechFromText( this.selectedVoice, this.selectedRate, this.text );
+
+	}
+	// I stop any current speech synthesis.
+	public stop() : void {
+
+		if ( speechSynthesis.speaking ) {
+			speechSynthesis.cancel();
+		}
+  }
+
+	// ---
+	// PRIVATE METHODS.
+	// ---
+	// I perform the low-level speech synthesis for the given voice, rate, and text.
+	private synthesizeSpeechFromText(
+		voice: SpeechSynthesisVoice,
+		rate: number,
+		text: string
+		) : void {
+
+		const utterance = new SpeechSynthesisUtterance( text );
+		utterance.voice = this.selectedVoice;
+    utterance.rate = rate;
+
+		speechSynthesis.speak( utterance );
+	}
 }
